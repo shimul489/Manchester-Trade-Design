ea69d6c5197ee550f844697697c91af320288fad
shimul489 <mdmahmudulshimul@gmail.com>
2023-05-03 23:09:21 +0600

    first

---
+++ assets/js/main.js
@@ -0,0 +1,115 @@
+(function($) { "use strict";
+
+ // niceSelect
+ $('select').niceSelect();
+
+// mobile-drop-down
+$(".main-nav .bi").on('click', function (event) {
+  var $fl = $(this);
+  $(this).parent().siblings().find('.sub-menu').slideUp();
+  $(this).parent().siblings().find('.bi').addClass('bi-chevron-down');
+  if ($fl.hasClass('bi-chevron-down')) {
+      $fl.removeClass('bi-chevron-down').addClass('bi-chevron-up');
+  } else {
+      $fl.removeClass('bi-chevron-up').addClass('bi-chevron-down');
+  }
+  $fl.next(".sub-menu").slideToggle();
+});
+
+
+// Home Two testimonial Slider
+var swiper = new Swiper(".h1-testimonial-slider", {
+  spaceBetween: 24,
+  slidesPerView: 1,
+  loop: true,
+  speed:2000,
+  autoplay: {
+    delay: 3000,
+  },
+  navigation: {
+    nextEl: ".next-btn-5",
+    prevEl: ".prev-btn-5",
+  },
+ 
+  // breakpoints: {
+  //   280:{
+  //     slidesPerView: 1,
+  //   },
+  //   480:{
+  //     slidesPerView: 1
+  //   },
+  //   768:{
+  //     slidesPerView: 1
+  //   },
+  //   992:{
+  //     slidesPerView: 1
+  //   },
+  //   1200:{
+  //     slidesPerView: 1
+  //   },
+  //   1400:{
+  //     slidesPerView:1
+  //   },
+  //   1600:{
+  //     slidesPerView: 1
+  //   },
+  // }
+});
+
+// mobile-search-area
+
+$('.search-btn').on("click", function(){
+  $('.mobile-search').addClass('slide');
+});
+
+$('.search-cross-btn').on("click", function(){
+  $('.mobile-search').removeClass('slide');
+});
+
+// Menu Toggle button sidebar
+var toggleIcon = document.querySelectorAll('.sidebar-btn')
+var closeIcon = document.querySelectorAll('.cross-icon')
+var searchWrap = document.querySelectorAll('.menu-toggle-btn-full-shape')
+
+toggleIcon.forEach((element)=>{
+  element.addEventListener('click', ()=>{
+      document.querySelectorAll('.menu-toggle-btn-full-shape').forEach((el)=>{
+          el.classList.add('show-sidebar')
+      })
+  })
+})
+closeIcon.forEach((element)=>{
+  element.addEventListener('click', ()=>{
+      document.querySelectorAll('.menu-toggle-btn-full-shape').forEach((el)=>{
+          el.classList.remove('show-sidebar')
+      })
+  })
+})
+
+window.onclick = function(event){
+  // Menu Toggle button sidebar
+  searchWrap.forEach((el)=>{
+    if(event.target === el){
+      el.classList.remove('show-sidebar')
+    }
+  })
+}
+document.querySelector('.sidebar-button').addEventListener('click', () => 
+document.querySelector('.main-nav').classList.toggle('show-menu'));
+
+$('.sidebar-button').click (function(){
+  $(this).toggleClass('active');
+});
+
+
+  // mobile-menu
+
+  $(".mobile-menu-btn").on("click", function () {
+    $(".main-nav").addClass("show-menu");
+  });
+
+  $(".menu-close-btn").on("click", function () {
+    $(".main-nav").removeClass("show-menu");
+  });
+
+}(jQuery));
